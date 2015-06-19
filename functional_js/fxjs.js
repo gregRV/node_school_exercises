29fa86a6581f4b6773d8c0a8b95b4144e98812ff
Greg RV <gregvarias@gmail.com>
2015-06-19 02:39:56 -0700

    done up to Reduce

--- functional_js/fxjs.js
+++ functional_js/fxjs.js
@@ -1,3 +1,4 @@
+// HELLO WORLD 
 function upperCaser(input) {
   // "why hello there"
   var word = input.split(' ');
@@ -9,5 +10,57 @@ function upperCaser(input) {
 
   return uppers.join(' ');
 }
-
 module.exports = upperCaser;
+
+
+// ORDER FUNCTIONS
+function repeat (operation, num) {
+  if (num > 0) {
+    operation();
+    repeat(operation, --num);
+  }
+}
+module.exports = repeat;
+
+
+// MAP
+function doubleAll (numbers) {
+  return Array.prototype.map.call(numbers, function(n){
+    return n * 2;
+  });
+}
+module.exports = doubleAll;
+
+
+// FILTER
+function getShortMessages (messages) {
+  return messages.filter(function(m){
+      return m.message.length < 50;
+    }).map(function(obj){
+      return obj.message;
+    });
+}
+module.exports = getShortMessages;
+
+
+// EVERY SOME
+function checkUsersValid (goodUsers) {
+  return function allUsersValid (submittedUsers) {
+    return submittedUsers.every(function(user){
+      return goodUsers.some(function(gU){
+        return gU.id === user.id;
+      });
+    })
+  }
+}
+module.exports = checkUsersValid;
+
+
+// REDUCE
+function countWords (inputWords) {
+  return inputWords.reduce(function(memo, word){
+    memo[word] ? memo[word] += 1 : memo[word] = 1;
+    return memo;
+  }, {});
+}
+module.exports = countWords;
